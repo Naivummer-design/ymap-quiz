aa707d900f000f62e24aa439c4e8c89600be0073
Ran JU <ran.25@intl.zju.edu.cn>
2026-04-20 21:04:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function getBasePath() {
 }
 
 export default defineConfig({
-  base: 'ymap-quiz',
+  base: '/ymap-quiz/',
   envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
   plugins: [react(), tailwindcss()],
   resolve: {
